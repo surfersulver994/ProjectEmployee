7b92b9a07479221f73d309226e78b44e288d3ab1
EARL DESOUZA <surfersilver994@gmial.com>
2020-06-16 23:47:51 +0530

    ProductModel

---
+++ ProjectDemo/Models/ProductModel.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace ProjectDemo.Models
+{
+    public class ProductModel
+    {
+        public int ProductCode { get; set; }
+        public string ProductName { get; set; }
+        public int Unit { get; set; }
+        public int Rate { get; set; }
+        public int Description { get; set; }
+        public int Title { get; set; }
+        public int ProductImage { get; set; }
+    }
+}
