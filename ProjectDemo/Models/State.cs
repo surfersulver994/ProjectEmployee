da1718b0c2a5ef701c2a733f814925453b62dd8c
EARL DESOUZA <surfersilver994@gmial.com>
2020-07-17 22:06:33 +0530

    Iquerayable efforts

--- ProjectDemo/Models/State.cs
+++ ProjectDemo/Models/State.cs
@@ -5,6 +5,7 @@ using System.Data.SqlClient;
 using System.Linq;
 using System.Web;
 using System.Web.Mvc;
+using System.Linq;
 
 namespace CascadingComboBox1.Models
 {
@@ -14,90 +15,90 @@ namespace CascadingComboBox1.Models
         public int StateID { get; set; }
         public string StateName { get; set; }
 
-        public static IQueryable<State> GetStates()
-        {
-            return new List<State>
-            {
-                new State
-                    {
-                        CountryCode = "CA",
-                        StateID=1,
-                        StateName = "Ontario"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=2,
-                        StateName = "Quebec"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=3,
-                        StateName = "Nova Scotia"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=4,
-                        StateName = "New Brunswick"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=5,
-                        StateName = "Manitoba"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=6,
-                        StateName = "British Columbia"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=7,
-                        StateName = "Prince Edward Island"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=8,
-                        StateName = "Saskatchewan"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=9,
-                        StateName = "Alberta"
-                    },
-                new State
-                    {
-                        CountryCode = "2",
-                        StateID=10,
-                        StateName = "Newfoundland and Labrador"
-                    },
-                new State
-                    {
-                        CountryCode = "2",
-                        StateID=11,
-                        StateName = "New-York"
-                    },
-                new State
-                    {
-                        CountryCode = "2",
-                        StateID=12,
-                        StateName = "California"
-                    },
-                new State
-                    {
-                        CountryCode = "2",
-                        StateID=13,
-                        StateName = "Washington"
-                    }
-            }.AsQueryable();
-        }
+        //public static IQueryable<State> GetStates()
+        //{
+        //    return new List<State>
+        //    {
+        //        new State
+        //            {
+        //                CountryCode = "CA",
+        //                StateID=1,
+        //                StateName = "Ontario"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "3",
+        //                StateID=2,
+        //                StateName = "Quebec"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "3",
+        //                StateID=3,
+        //                StateName = "Nova Scotia"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "3",
+        //                StateID=4,
+        //                StateName = "New Brunswick"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "3",
+        //                StateID=5,
+        //                StateName = "Manitoba"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "3",
+        //                StateID=6,
+        //                StateName = "British Columbia"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "3",
+        //                StateID=7,
+        //                StateName = "Prince Edward Island"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "3",
+        //                StateID=8,
+        //                StateName = "Saskatchewan"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "3",
+        //                StateID=9,
+        //                StateName = "Alberta"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "2",
+        //                StateID=10,
+        //                StateName = "Newfoundland and Labrador"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "2",
+        //                StateID=11,
+        //                StateName = "New-York"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "2",
+        //                StateID=12,
+        //                StateName = "California"
+        //            },
+        //        new State
+        //            {
+        //                CountryCode = "2",
+        //                StateID=13,
+        //                StateName = "Washington"
+        //            }
+        //    }.AsQueryable();
+        //}
 
 
         //public static IQueryable<State> SqlGetStates()
@@ -191,30 +192,32 @@ namespace CascadingComboBox1.Models
         //    }.AsQueryable();
         //}
 
-        //public static IQueryable<State> GetStates()
-        //{
-        //    string connectionString = @"data source=DESKTOP-7R1I2HK; initial catalog=NEWTEMPDB; integrated security=True; MultipleActiveResultSets=True";
-        //    using (SqlConnection sqlCon = new SqlConnection(connectionString))
-        //    {
-        //        ////////viewbag code start
-        //        DataTable dtblProductCode = new DataTable();
-        //        SqlDataAdapter sqlDaa = new SqlDataAdapter("SELECT [Rate] FROM [NEWTEMPDB].[dbo].[Product]", sqlCon);
-        //        sqlDaa.Fill(dtblProductCode);
-        //        List<int> ProductCodeList = new List<int>();
-        //        foreach (DataRow dr in dtblProductCode.Rows)
-        //        {
-        //            int StateID = dr.Field<int>("Rate");
-        //            int StateName = dr.Field<int>("Rate");
-        //            ProductCodeList.Add(StateID);
-        //            ProductCodeList.Add(StateName);
-        //            SelectList list = new SelectList(ProductCodeList, "", "");
-        //            ////////ViewBag.ProductCodeList = list;
+        public static IQueryable<State> GetStates()
+        {
+            string connectionstring = @"data source=desktop-7r1i2hk; initial catalog=newtempdb; integrated security=true; multipleactiveresultsets=true";
+            using (SqlConnection sqlcon = new SqlConnection(connectionstring))
+            {
+                ////////viewbag code start
+                DataTable dtblproductcode = new DataTable();
+                SqlDataAdapter sqldaa = new SqlDataAdapter("select [rate] from [newtempdb].[dbo].[product]", sqlcon);
+                sqldaa.Fill(dtblproductcode);
+                List<int> productcodelist = new List<int>();
+                foreach (DataRow dr in dtblproductcode.Rows)
+                {
+                    int stateid = dr.Field<int>("rate");
+                    int statename = dr.Field<int>("rate");
+                    productcodelist.Add(stateid);
+                    productcodelist.Add(statename);
+                    SelectList list = new SelectList(productcodelist, "", "");
+                    ////////viewbag.productcodelist = list;
 
-        //        }
-        //        ////////viewbag code end
-        //    }
-        //    return new List<State>();
-        //}
+                }
+                ////////viewbag code end
+                var Aqueryable = productcodelist.AsQueryable();
+                return list<Aqueryable>
+            }
+
+        }
 
 
 
