613c92f2236fb55755424e0ee31298506393d00d
EARL DESOUZA <surfersilver994@gmial.com>
2020-07-18 16:15:22 +0530

    A

--- ProjectDemo/Models/State.cs
+++ ProjectDemo/Models/State.cs
@@ -11,88 +11,11 @@ public class State
     {
         return new List<State>
             {
-                new State
-                    {
-                        CountryCode = "CA",
-                        StateID=1,
-                        StateName = "Ontario"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=2,
-                        StateName = "Quebec"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=3,
-                        StateName = "Nova Scotia"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=4,
-                        StateName = "New Brunswick"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=5,
-                        StateName = "Manitoba"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=6,
-                        StateName = "British Columbia"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=7,
-                        StateName = "Prince Edward Island"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=8,
-                        StateName = "Saskatchewan"
-                    },
-                new State
-                    {
-                        CountryCode = "3",
-                        StateID=9,
-                        StateName = "Alberta"
-                    },
-                new State
-                    {
-                        CountryCode = "2",
-                        StateID=10,
-                        StateName = "Newfoundland and Labrador"
-                    },
-                new State
-                    {
-                        CountryCode = "2",
-                        StateID=11,
-                        StateName = "New-York"
-                    },
-                new State
-                    {
-                        CountryCode = "2",
-                        StateID=12,
-                        StateName = "California"
-                    },
-                new State
-                    {
-                        CountryCode = "2",
-                        StateID=13,
-                        StateName = "Washington"
-                    }
-            }.AsQueryable();
+            var a = new List<string>();
+            var queryable = list.AsQueryable();
     }
 
-    
+
 
 
     public static IQueryable<State> SqlGetStates()
