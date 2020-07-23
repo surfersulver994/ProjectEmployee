c54825da2dbaa999f688cf50e8cdfb5182135276
EARL DESOUZA <surfersilver994@gmial.com>
2020-07-23 20:26:08 +0530

    Returning List To  IQueryable

--- ProjectDemo/Models/State.cs
+++ ProjectDemo/Models/State.cs
@@ -1,4 +1,6 @@
-﻿using System.Collections.Generic;
+﻿using System;
+using System.Collections.Generic;
+using System.Data.SqlClient;
 using System.Linq;
 
 public class State
@@ -7,77 +9,81 @@ public class State
     public int StateID { get; set; }
     public string Unit { get; set; }
     public int Rate { get; set; }
+    public string Image { get; set; }
+
+    //public static IQueryable<State> GetStates()
+    //{
+    //    return new List<State>
+    //        {
+    //            new State
+    //                {
+    //                    ProductCode = "2",
+    //                    StateID=99,
+    //                    Unit = "pieces"//bring in unit
+    //                },
+    //            new State
+    //                {
+    //                    ProductCode = "2",
+    //                    StateID=12,
+    //                    Unit = "56"//bring in rate
+    //                },
+    //            new State
+    //                {
+    //                    ProductCode = "2",
+    //                    StateID=13,
+    //                    Unit = "~/Image/a201101509.png"//bring in image
+    //                },
+    //            new State
+    //                {
+    //                    ProductCode = "3",
+    //                    StateID=11,
+    //                    Unit = "kilos"//bring in unit
+    //                },
+    //               new State
+    //                {
+    //                    ProductCode = "3",
+    //                    StateID=11,
+    //                    Unit = "ounces"//bring in rate
+    //                },
+    //                  new State
+    //                {
+    //                    ProductCode = "3",
+    //                    StateID=11,
+    //                    Unit = "~/Image/a201101509.png"//bring in image
+    //                }
+
+    //        }.AsQueryable();
+    //}
 
 
     public static IQueryable<State> GetStates()
     {
-        return new List<State>
-            {   
-                new State
-                    {
-                        ProductCode = "2",
-                        StateID=99,
-                        Unit = "pieces"//bring in unit
-                    },
-                new State
-                    {
-                        ProductCode = "2",
-                        StateID=12,
-                        Unit = "56"//bring in rate
-                    },
-                new State
-                    {
-                        ProductCode = "2",
-                        StateID=13,
-                        Unit = "~/Image/a201101509.png"//bring in image
-                    },
-                new State
-                    {
-                        ProductCode = "3",
-                        StateID=11,
-                        Unit = "kilos"//bring in unit
-                    },
-                   new State
-                    {
-                        ProductCode = "3",
-                        StateID=11,
-                        Unit = "ounces"//bring in rate
-                    },
-                      new State
-                    {
-                        ProductCode = "3",
-                        StateID=11,
-                        Unit = "~/Image/a201101509.png"//bring in image
-                    }
+        string queryString = "SELECT [ProductCode],[ProductName],[Unit],[Rate],[Description],[ProductImage]FROM [NEWTEMPDB].[dbo].[Product]";
+        string connectionString = @"data source=DESKTOP-7R1I2HK; initial catalog=NEWTEMPDB; integrated security=True; MultipleActiveResultSets=True";
+        List<State> families = new List<State>();
+        using (SqlConnection myConnection = new SqlConnection(connectionString))
+        {
+            
+            myConnection.Open();
+            SqlCommand command = new SqlCommand(queryString, myConnection);
+            SqlDataReader nwReader = command.ExecuteReader();
+            while (nwReader.Read())
+            {
+                families.Add(new State
+                {
+                    Unit  = nwReader["Unit"].ToString(),
+                    Rate  = Convert.ToInt32(nwReader["Rate"].ToString()),
+                    Image = nwReader["ProductImage"].ToString(),
+                });
+            }
 
-            }.AsQueryable();
+            myConnection.Close();
+
+        }
+        var showFamilies = families.ToList();
+        IQueryable getFamilies = showFamilies.AsQueryable();
+
+        return (IQueryable<State>)getFamilies;
     }
-
-    //public static IQueryable<State> GetStates()
-    //{
-    //    var a = "branch2";
-    //    string connectionstring = @"data source=desktop-7r1i2hk; initial catalog=newtempdb; integrated security=true; multipleactiveresultsets=true";
-    //    using (SqlConnection sqlcon = new SqlConnection(connectionstring))
-    //    {
-    //        ////////viewbag code start
-    //        DataTable dtblproductcode = new DataTable();
-    //        SqlDataAdapter sqldaa = new SqlDataAdapter("select [rate] from [newtempdb].[dbo].[product]", sqlcon);
-    //        sqldaa.Fill(dtblproductcode);
-    //        List<int> productcodelist = new List<int>();
-    //        foreach (DataRow dr in dtblproductcode.Rows)
-    //        {
-    //            int stateid = dr.Field<int>("rate");
-    //            int statename = dr.Field<int>("rate");
-    //            productcodelist.Add(stateid);
-    //            productcodelist.Add(statename);
-    //            SelectList list = new SelectList(productcodelist, "", "");
-    //            ////////viewbag.productcodelist = list;
-
-    //        }
-    //        ////////viewbag code end
-    //        var Aqueryable = productcodelist.AsQueryable();
-    //        return List<Aqueryable>();
-
-    //    }
 
 }
